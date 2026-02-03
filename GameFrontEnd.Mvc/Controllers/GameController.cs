90590a4f1fda3396e9e587497d6ea02d67884143
Saurabh880 <backtohome924@gmal.com>
2026-02-03 12:17:35 +0530

    v1.1 - Updated Game Controller - Added Service calls removed HTTP Client Code

--- GameFrontEnd.Mvc/Controllers/GameController.cs
+++ GameFrontEnd.Mvc/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using GameWebAPI.DTO;
+﻿using GameFrontEnd.Mvc.Service;
+using GameWebAPI.DTO;
 using GameWebAPI.Entites;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -8,103 +9,64 @@ namespace GameFrontEnd.Mvc.Controllers
 {
     public class GameController : Controller
     {
-        private string baseUrl = "http://localhost:5140/";
         private  List<Genre> allGenre;
+
+        private readonly IGameService _gameService;
+
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
 
         public async Task<IActionResult> Index()
         {
-            List<GameReadDTO> game = new List<GameReadDTO>();
-
-            //API call
-            using (var _httpclient = new HttpClient())
+            try
             {
-                _httpclient.BaseAddress = new Uri(baseUrl+"api/v1/Game");
-                _httpclient.DefaultRequestHeaders.Accept.Clear();
-                _httpclient.DefaultRequestHeaders.Accept.Add
-                    (
-                        new MediaTypeWithQualityHeaderValue("application/json")
-                    );
-
-                //Get all games
-                HttpResponseMessage getData = await _httpclient.GetAsync("");
-                if(getData.IsSuccessStatusCode)
-                {
-                    string result = getData.Content.ReadAsStringAsync().Result;
-                    game = JsonConvert.DeserializeObject<List<GameReadDTO>>(result);
-                }
-                else
-                {
-                    return View("Error");
-                }
-
+                var games = await _gameService.GetGamesAsync();
+                return View(games);
             }
-            return View(game);
+            catch
+            {
+                return View("Error");
+            }
         }
+
         public async Task<IActionResult> CreateGame()
         {
-            allGenre = new List<Genre>();
-            using (var _httpclient = new HttpClient())
+            try
             {
-                
-                _httpclient.BaseAddress = new Uri(baseUrl + "api/v1/Genre");
-                _httpclient.DefaultRequestHeaders.Accept.Clear();
-                _httpclient.DefaultRequestHeaders.Accept.Add
-                    (
-                        new MediaTypeWithQualityHeaderValue("applicatiion/json")
-                    );
-
-                //Get all games
-                HttpResponseMessage getData = await _httpclient.GetAsync("");
-                if (getData.IsSuccessStatusCode)
-                {
-                    string result = getData.Content.ReadAsStringAsync().Result;
-                    allGenre = JsonConvert.DeserializeObject<List<Genre>>(result);
-                    ViewBag.Genre = allGenre;
-                }
-                else
-                {
-                    return View("Error");
-                }
+                var genres = await _gameService.GetGenresAsync();
+                ViewBag.Genre = genres;
                 return View();
             }
+            catch
+            {
+                return View("Error");
+            }
         }
+
+        [HttpPost]
         public async Task<IActionResult> CreateNewGame(GameCreateDTO createGame)
         {
             if (!ModelState.IsValid)
             {
-                ViewBag.errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                ViewBag.errors = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
                 return View();
             }
-            if(createGame == null) throw new ArgumentNullException(nameof(createGame));
 
-            //Validate the Person Name
-            if (string.IsNullOrEmpty(createGame.GameName))
+            if (string.IsNullOrEmpty(createGame?.GameName))
             {
-                throw new ArgumentException("Game name can't be null");
-            }
-            using (var _httpclient = new HttpClient())
-            {
-                _httpclient.BaseAddress = new Uri(baseUrl + "api/v1/Game");
-                _httpclient.DefaultRequestHeaders.Accept.Clear();
-                _httpclient.DefaultRequestHeaders.Accept.Add
-                    (
-                        new MediaTypeWithQualityHeaderValue("applicatiion/json")
-                    );
-
-                //Get all games
-                HttpResponseMessage postData = await _httpclient.PostAsJsonAsync("", createGame);
-                if (postData.IsSuccessStatusCode)
-                {
-                    return RedirectToAction("Index");
-                }
-                else
-                {
-                    return View("Error");
-                }
-
+                ModelState.AddModelError("GameName", "Game name can't be null");
+                return View();
             }
 
+            var success = await _gameService.CreateGameAsync(createGame);
+            return success ? RedirectToAction("Index") : View("Error");
         }
+
 
         public IActionResult Error()
         {
